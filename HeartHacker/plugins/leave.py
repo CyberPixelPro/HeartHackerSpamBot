a22ccca0a1150ecd25b27a62381b7e3111cfa05c
AT WORLD'S END <87578989+AT-WORLDS-END@users.noreply.github.com>
2022-01-26 11:16:52 +0530

    Update leave.py

--- HeartHacker/plugins/leave.py
+++ HeartHacker/plugins/leave.py
@@ -1,5 +1,6 @@
 import asyncio
-from .. import Riz, Riz2, Riz3, Riz4, Riz5, Riz6, Riz7, Riz8, Riz9, Riz10, SUDO_USERS
+from HeartHacker import Riz, Riz2, Riz3, Riz4, Riz5, Riz6, Riz7, Riz8, Riz9, Riz10, SUDO_USERS
+from HeartHacker import CMD_HNDLR as hl
 import telethon.utils
 from telethon.tl import functions
 from telethon.tl.functions.channels import LeaveChannelRequest
@@ -7,31 +8,30 @@ from telethon import events
 import os
 import random
 import sys
-    
 
-@Riz.on(events.NewMessage(pattern=r"\.leave"))
-@Riz2.on(events.NewMessage(pattern=r"\.leave"))
-@Riz3.on(events.NewMessage(pattern=r"\.leave"))
-@Riz4.on(events.NewMessage(pattern=r"\.leave"))
-@Riz5.on(events.NewMessage(pattern=r"\.leave"))
-@Riz6.on(events.NewMessage(pattern=r"\.leave"))
-@Riz7.on(events.NewMessage(pattern=r"\.leave"))
-@Riz8.on(events.NewMessage(pattern=r"\.leave"))
-@Riz9.on(events.NewMessage(pattern=r"\.leave"))
-@Riz10.on(events.NewMessage(pattern=r"\.leave"))
+@Riz.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@Riz2.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@Riz3.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@Riz4.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@Riz5.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@Riz6.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@Riz7.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@Riz8.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@Riz9.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@Riz10.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
 async def _(e):
     usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗟𝗲𝗮𝘃𝗲\n\nCommand:\n\n.leave <Channel or Chat ID>"
     if e.sender_id in SUDO_USERS:
         rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         if len(e.text) > 7:
             bc = rizoel[0]
-            bc = int(bc)
+            Xd = int(bc)
             text = "Leaving....."
             event = await e.reply(text, parse_mode=None, link_preview=None )
             try:
-                await event.client(LeaveChannelRequest(bc))
-                await event.edit("Succesfully Left")
+                await event.client(LeaveChannelRequest(Xd))
+                await event.edit("Succesfully Left ✅")
             except Exception as e:
                 await event.edit(str(e))   
         else:
-            await e.reply(usage, parse_mode=None, link_preview=None )
+            await e.reply(usage, parse_mode=None, link_preview=None )   
